4149b3f269ebb531a47550be459ec64634d7a735
Herminigildo <herminigildo.timtim@cit.edu>
2022-12-16 16:51:00 +0800

    Add exception handling to FoodOrder

--- src/FoodOrdering/FoodOrderGUI.java
+++ src/FoodOrdering/FoodOrderGUI.java
@@ -1,6 +1,12 @@
 package FoodOrdering;
 
+import LeapYear.LeapYearGUI;
+
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 public class FoodOrderGUI extends JFrame {
     private JPanel panel1;
@@ -17,12 +23,61 @@ public class FoodOrderGUI extends JFrame {
     private JRadioButton rb15;
 
     public FoodOrderGUI(){
+        panel1.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
 
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if(e.getKeyCode()== KeyEvent.VK_ENTER){
+                    calculator();
+                }
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+
+            }
+        });
+        btnOrder.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                calculator();
+            }
+        });
     }
 
     public void calculator(){
         try{
             double res = 0;
+            if(cPizza.isSelected()){
+                res+=100;
+            }
+            if(cBurger.isSelected()){
+                res+=80;
+            }
+            if(cFries.isSelected()){
+                res+=65;
+            }
+            if(cSoftDrinks.isSelected()){
+                res+=55;
+            }
+            if(cTea.isSelected()){
+                res+=50;
+            }
+            if(cSundae.isSelected()){
+                res+=40;
+            }
+
+            if(rb5.isSelected()){
+                res *=0.5;
+            }else if(rb10.isSelected()){
+                res*=0.10;
+            }else if(rb15.isSelected()){
+                res*=0.15;
+            }
 
 
         }catch (Exception hermi){
@@ -31,7 +86,12 @@ public class FoodOrderGUI extends JFrame {
     }
 
     public static void main(String[] args) {
-
+        FoodOrderGUI app = new FoodOrderGUI();
+        app.setTitle("Leap Year Checker");
+        app.setContentPane(app.panel1);
+        app.setVisible(true);
+        app.setSize(300,200);
+        app.setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 
 }

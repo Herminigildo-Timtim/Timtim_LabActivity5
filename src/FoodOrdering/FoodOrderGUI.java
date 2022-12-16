0f3e7e845c03ff4a464e979fbfccec6cc7503d9e
Herminigildo <herminigildo.timtim@cit.edu>
2022-12-16 16:32:40 +0800

    Rename FoodOrder components

--- src/FoodOrdering/FoodOrderGUI.java
+++ src/FoodOrdering/FoodOrderGUI.java
@@ -4,7 +4,16 @@ import javax.swing.*;
 
 public class FoodOrderGUI {
     private JPanel panel1;
-    private JCheckBox pizzaCheckBox;
-    private JRadioButton noneRadioButton;
-    private JButton orderButton;
+    private JCheckBox cPizza;
+    private JRadioButton rbNone;
+    private JButton btnOrder;
+    private JCheckBox cBurger;
+    private JCheckBox cFries;
+    private JCheckBox cSoftDrinks;
+    private JCheckBox cTea;
+    private JCheckBox cSundae;
+    private JRadioButton rb5;
+    private JRadioButton rb10;
+    private JRadioButton rb15;
+
 }
